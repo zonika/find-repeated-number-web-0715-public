a26c16d473b0510e0b5cc6185eb7cd294eec2d9d
Katie Hoffman <ktahoffman@gmail.com>
2014-09-15 14:07:51 -0400

    fix second test

--- spec/find_repeated_number_spec.rb
+++ spec/find_repeated_number_spec.rb
@@ -1,6 +1,6 @@
 describe '#find_repeated_number' do
   let(:first_array) { [1,2,3,4,5,5,6,7] }
-  let(:second_array) { [129, 54, 36, 104, 45, 232, 87, 151, 139, 97, 43, 193, 136, 108, 229, 3, 151, 195, 261, 191, 97] }
+  let(:second_array) { [129, 54, 36, 104, 45, 232, 87, 139, 97, 43, 193, 136, 108, 229, 3, 151, 195, 261, 191, 97] }
   
   it 'returns the repeated number for single digit numbers' do
     expect(find_repeated_number(first_array)).to eq(5)
